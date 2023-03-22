d9fce00f6442480da87fd8b994016a0106012836
Braden Byrnes <56095224+Pufferfish3000@users.noreply.github.com>
2023-03-22 13:10:23 -0400

    initial commit

---
+++ src/PythonScanner/control.py
@@ -0,0 +1,15 @@
+"""
+Main controller
+
+@author: Byrnes
+"""
+
+import re
+
+"""
+main function to run, gathers input from user to determine what the user wants
+to user the program for
+"""
+def main():
+
+main()
