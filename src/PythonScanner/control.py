ad07d82adf61502dbc676c4ec260b10f2b735ed6
Braden Byrnes <56095224+Pufferfish3000@users.noreply.github.com>
2023-03-22 15:06:14 -0400

    added general setup
    -added __pycache__ to .gitignore
    -fixed general english issues
    -added untested code to test port connection

--- src/PythonScanner/control.py
+++ src/PythonScanner/control.py
@@ -1,15 +1,17 @@
+#!/usr/bin/env python3
 """
 Main controller
 
 @author: Byrnes
 """
 
-import re
+import argparse
+import scan
 
 """
-main function to run, gathers input from user to determine what the user wants
-to user the program for
+gathers input from user to determine what functions to run
 """
 def main():
-
+    params = str(input())
+    
 main()
--- src/PythonScanner/scan.py
+++ src/PythonScanner/scan.py
@@ -1,6 +1,19 @@
+#!/usr/bin/env python3
 """
 Main scanner functions
 
 @author: Byrnes
 """
 
+import socket
+
+def checkPort(source, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    try:
+        s.connect((source, port))
+        return (True)
+    except socket.error as error:
+        return (False)
+    finally:
+        s.close()
