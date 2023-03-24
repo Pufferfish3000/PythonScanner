8dc2d46ae417d82eb6ce166a793ede7711e75417
Braden Byrnes <56095224+Pufferfish3000@users.noreply.github.com>
2023-03-24 15:16:56 -0400

    added better parseArgs implementation

--- src/PythonScanner/control.py
+++ src/PythonScanner/control.py
@@ -14,5 +14,20 @@ def main():
     gathers input from user to determine what functions to run
     """
 
+    parser = argparse.ArgumentParser(description="Simple tcp and udp scanner")
+    scanType = parser.add_mutually_exclusive_group()
+    parser.add_argument("address", type=str, dest="targetAddr",
+                        help="The ipv4 address or domain of your target")
+    scanType.add_argument("--tcp-scan", "-t", type=str, action="store_true",
+                          dest="tcpScan", help="Tcp connect scan")
+    scanType.add_argument("--udp-scan", "-u", type=str, action="store_true",
+                          dest="udpScan", help="Udp scan")
+    args = parser.parse_args()
 
-main()
+    print(args)
+
+    scanner = scan.PortScanner(args)
+
+
+if __name__ == "__main__":
+    main()
